3a158a48aac8501f29a339cc079ea34c42c86e1c
abdellah-darni <abdellah.darni.2018@gmail.com>
2025-12-26 15:41:29 +0100

    Test : test the module linking

---
+++ server/DeepDame/src/test/java/com/deepdame/enginetest/EngineIntegrationTest.java
@@ -0,0 +1,18 @@
+package com.deepdame.enginetest;
+
+import com.deepdame.engine.core.Board;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+
+public class EngineIntegrationTest {
+    @Test
+    void testBackendCanSeeEngine() {
+        Board board = new Board();
+        String status = board.getBoardState();
+
+        assertEquals("I am the Game Engine Board! Logic is working.", status);
+        System.out.println("✅ MODULE LINKING VERIFIED: Backend read data from Engine.");
+    }
+}
