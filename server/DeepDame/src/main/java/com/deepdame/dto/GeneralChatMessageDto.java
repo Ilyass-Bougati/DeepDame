53bf1d6a964223eaed2b6077ec905b61ca8df61b
Ilyass bougati <i.bougati12@gmail.com>
2025-12-26 17:40:46 +0100

    Server: Added DTOs and Repositories

--- server/DeepDame/src/main/java/com/deepdame/dto/GeneralChatMessageDto.java
+++ server/DeepDame/src/main/java/com/deepdame/dto/GeneralChatMessageDto.java
@@ -1,24 +1,19 @@
-package com.deepdame.entity;
+package com.deepdame.dto;
 
-import jakarta.persistence.*;
 import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.NotNull;
 import lombok.*;
-import org.hibernate.annotations.CreationTimestamp;
 
 import java.time.LocalDateTime;
 import java.util.UUID;
 
-@Entity
 @Builder
 @Getter
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
 @EqualsAndHashCode(onlyExplicitlyIncluded = true)
-public class GeneralChatMessage {
-    @Id
-    @GeneratedValue(strategy = GenerationType.UUID)
+public class GeneralChatMessageDto {
     @EqualsAndHashCode.Include
     private UUID id;
 
@@ -26,10 +21,5 @@ public class GeneralChatMessage {
     private String message;
 
     @NotNull
-    @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "user_id")
-    private User user;
-
-    @CreationTimestamp
     private LocalDateTime createdAt;
 }
