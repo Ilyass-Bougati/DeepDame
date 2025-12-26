2ff2dcbddee267089f8df776f0ea6e417a83eb4e
Ilyass bougati <i.bougati12@gmail.com>
2025-12-26 17:04:08 +0100

    Server: Added the entities

---
+++ server/DeepDame/src/main/java/com/deepdame/entity/User.java
@@ -0,0 +1,58 @@
+package com.deepdame.entity;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Email;
+import lombok.*;
+import org.hibernate.annotations.CreationTimestamp;
+
+import java.time.LocalDateTime;
+import java.util.*;
+
+@Entity
+@Table(name = "users")
+@Builder
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    @EqualsAndHashCode.Include
+    private UUID id;
+
+    @Column(unique = true)
+    private String username;
+    private String password;
+
+    @Email
+    @Column(unique = true)
+    private String email;
+
+    private Boolean emailValidated = false;
+    private Boolean bannedFromChat = false;
+    private Boolean bannedFromApp  = false;
+
+    @ManyToMany(fetch = FetchType.LAZY)
+    @JoinTable(
+            name = "user_friends",
+            joinColumns = @JoinColumn(name = "user_id"),
+            inverseJoinColumns = @JoinColumn(name = "friend_id")
+    )
+    private Set<User> friends = new HashSet<>();
+
+    @OneToMany(mappedBy = "user", fetch = FetchType.LAZY, cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<GeneralChatMessage> messages = new ArrayList<>();
+
+    @ManyToMany(fetch = FetchType.EAGER)
+    @JoinTable(
+            name = "user_roles",
+            joinColumns = @JoinColumn(name = "user_id"),
+            inverseJoinColumns = @JoinColumn(name = "role_id")
+    )
+    private Set<Role> roles = new HashSet<>();
+
+    @CreationTimestamp
+    private LocalDateTime createdAt;
+}
