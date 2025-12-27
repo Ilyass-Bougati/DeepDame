3ecd3ef5569a88d0be5b05839c69a5d00c667406
Hajar ELHILALI <hajarelhilali57@gmail.com>
2025-12-27 16:59:24 +0100

    add getLegalMoves & isValidMove

--- server/game-engine/src/main/java/com/deepdame/engine/core/logic/MoveValidator.java
+++ server/game-engine/src/main/java/com/deepdame/engine/core/logic/MoveValidator.java
@@ -10,8 +10,10 @@ public class MoveValidator {
 
     // the main function : cchecks if a specific move is allowed
     public boolean isValidMove(Board board, Move move, PieceType player){
+        if (move == null) return false;
+        List<Move> moveList = getLegalMoves(board, player);
 
-        return true;
+        return moveList.contains(move);
     }
 
 
@@ -21,10 +23,58 @@ public class MoveValidator {
         List<Move> slides = new ArrayList<>();
 
 
+        for(int row = 0; row < board.SIZE; row++){
+            for(int col = 0; col < board.SIZE; col++){
+                Position pos = new Position(row, col);
+                Piece piece = board.getPiece(pos);
+                if(piece != null && piece.type().equals(player)){
+                    findMovesForPiece(board, pos, piece, jumps, slides);
+                }
+            }
+
+        }
+
+
         // foreced jump rule
         if (!jumps.isEmpty()) {
             return jumps;
         }
         return slides;
     }
+
+    private int[][] getDirections(Piece piece){
+        if (piece.isKing()) {
+            return new int[][]{{-1, -1}, {-1, 1}, {1, -1}, {1, 1}};
+        }
+        if (piece.type() == PieceType.WHITE) {
+            return new int[][]{{1, -1}, {1, 1}};
+        } else {
+            return new int[][]{{-1, -1}, {-1, 1}};
+        }
+    }
+
+    private void findMovesForPiece(Board board, Position pos, Piece piece, List<Move> jumps, List<Move> slides){
+        int[][] directions = getDirections(piece);
+        for(int[] direction : directions){
+            int dRow = direction[0];
+            int dCol = direction[1];
+
+            Position next = new Position(pos.row() + dRow, pos.col() + dCol);
+            Position landing = new Position(pos.row() + (2 * dRow), pos.col() + (2 * dCol));
+            Piece middlePiece = board.getPiece(next);
+            Piece landingPiece = board.getPiece(landing);
+
+            if (middlePiece != null && !middlePiece.type().equals(piece.type())) {
+
+                if (board.isValidBounds(landing) && landingPiece == null) {
+                    jumps.add(new Move(pos, landing));
+                }
+            }
+
+            if (board.isValidBounds(next) && board.getPiece(next) == null) {
+                slides.add(new Move(pos, next));
+            }
+        }
+    }
+
 }
