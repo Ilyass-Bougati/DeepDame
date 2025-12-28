8f01e99b9af6bf88cb64bd7c6cb76552aae9fe8a
abdellah-darni <abdellah.darni.2018@gmail.com>
2025-12-28 21:27:44 +0100

    game service interface

---
+++ server/DeepDame/src/main/java/com/deepdame/service/game/GameService.java
@@ -0,0 +1,20 @@
+package com.deepdame.service.game;
+
+import com.deepdame.dto.game.GameDto;
+import com.deepdame.engine.core.model.Move;
+import com.deepdame.enums.GameMode;
+import com.deepdame.service.CrudDtoService;
+
+import java.util.List;
+import java.util.UUID;
+
+public interface GameService extends CrudDtoService<UUID, GameDto> {
+
+    GameDto createGame(UUID playerId, GameMode mode);
+    GameDto joinGame(UUID gameId, UUID playerId);
+    GameDto makeMove(UUID gameId, UUID playerId, Move move);
+    GameDto surrenderGame(UUID gameId, UUID playerId);
+
+    List<GameDto> getOpenGames();
+    List<GameDto> getUserGames(UUID playerId);
+}
