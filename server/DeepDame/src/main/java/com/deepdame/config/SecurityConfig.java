dcda7408cfedc292add0eb36629e91ac35a8cf37
Hajar ELHILALI <hajarelhilali57@gmail.com>
2025-12-28 21:57:51 +0100

    add admin login

--- server/DeepDame/src/main/java/com/deepdame/config/SecurityConfig.java
+++ server/DeepDame/src/main/java/com/deepdame/config/SecurityConfig.java
@@ -5,6 +5,7 @@ import com.deepdame.security.CustomUserDetailsService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.Order;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
@@ -34,6 +35,30 @@ public class SecurityConfig {
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .addFilterBefore(jwtCookieFilter, UsernamePasswordAuthenticationFilter.class)
                 .httpBasic(AbstractHttpConfigurer::disable);
+
+        return http.build();
+    }
+    @Bean
+    @Order(1)
+    public SecurityFilterChain adminSecurity(HttpSecurity http) throws Exception {
+        http
+                .securityMatcher("/admin/**", "/css/**", "/js/**")
+                .csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers("/admin/login", "/css/**", "/js/**").permitAll()
+                        .anyRequest().hasAuthority("ROLE_ADMIN")
+                )
+                .formLogin(form -> form
+                        .loginPage("/admin/login")
+                        .loginProcessingUrl("/admin/perform_login")
+                        .usernameParameter("email")
+                        .defaultSuccessUrl("/admin", true)
+                        .failureUrl("/admin/login?error=true")
+                )
+                .logout(logout -> logout
+                        .logoutUrl("/admin/logout")
+                        .logoutSuccessUrl("/admin/login?logout=true")
+                );
 
         return http.build();
     }
