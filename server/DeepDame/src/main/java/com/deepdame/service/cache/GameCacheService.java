4dedd68978456a6398437ce9b29f3a21bfde1ee5
abdellah-darni <abdellah.darni.2018@gmail.com>
2026-01-01 18:42:28 +0100

    adding a redis cache lobby to store all open games instead of idk lopping on all games and check if there open ig

--- server/DeepDame/src/main/java/com/deepdame/service/cache/GameCacheService.java
+++ server/DeepDame/src/main/java/com/deepdame/service/cache/GameCacheService.java
@@ -3,8 +3,12 @@ package com.deepdame.service.cache;
 import com.deepdame.entity.GameDocument;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Set;
 import java.util.UUID;
 import java.util.concurrent.TimeUnit;
 
@@ -14,7 +18,10 @@ public class GameCacheService {
 
     private final RedisTemplate<String, GameDocument> gameTemplate;
 
+    private final StringRedisTemplate stringTemplate;
+
     private static final String KEY_GAME = "game:";
+    private static final String KEY_LOBBY = "lobby:open_games";
 
     public void saveGame(GameDocument game){
 
@@ -33,5 +40,25 @@ public class GameCacheService {
         String key = KEY_GAME + gameId;
 
         gameTemplate.delete(key);
+    }
+
+    public void addToLobby(UUID gameId){
+        stringTemplate.opsForSet().add(KEY_LOBBY, gameId.toString());
+    }
+
+    public void removeFromLobby(UUID gameId){
+        stringTemplate.opsForSet().remove(KEY_LOBBY, gameId.toString());
+    }
+
+    public List<GameDocument> getOpenGames(){
+
+        Set<String> ids = stringTemplate.opsForSet().members(KEY_LOBBY);
+
+        List<GameDocument> gameDocuments = new ArrayList<>();
+
+        for (String id : ids){
+            gameDocuments.add(getGame(UUID.fromString(id)));
+        }
+        return gameDocuments;
     }
 }
