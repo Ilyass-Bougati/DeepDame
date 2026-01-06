973a21392e6162adb44928bb511acdf38709c9f1
Ilyass bougati <i.bougati12@gmail.com>
2026-01-06 11:11:52 +0100

    Paginating the fetching general chat messags

--- server/DeepDame/src/main/java/com/deepdame/service/generalChatMessage/GeneralChatMessageService.java
+++ server/DeepDame/src/main/java/com/deepdame/service/generalChatMessage/GeneralChatMessageService.java
@@ -7,5 +7,5 @@ import java.util.List;
 import java.util.UUID;
 
 public interface GeneralChatMessageService extends CrudDtoService<UUID, GeneralChatMessageDto> {
-    List<GeneralChatMessageDto> getGeneralChatMessages(Long limit);
+    List<GeneralChatMessageDto> getGeneralChatMessages(Long pageNumber, Long limit);
 }
