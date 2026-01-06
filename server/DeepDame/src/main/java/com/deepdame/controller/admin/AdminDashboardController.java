7a4f857e0f5b1a4c6f5182d8ef2bffad5f1f9f2c
abdellah-darni <abdellah.darni.2018@gmail.com>
2026-01-06 19:20:51 +0100

    the admin dashboard stats

--- server/DeepDame/src/main/java/com/deepdame/controller/admin/AdminDashboardController.java
+++ server/DeepDame/src/main/java/com/deepdame/controller/admin/AdminDashboardController.java
@@ -1,14 +1,22 @@
 package com.deepdame.controller.admin;
 
+import com.deepdame.service.statistic.StatisticsServiceImpl;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("/admin")
+@RequiredArgsConstructor
 public class AdminDashboardController {
+
+    private final StatisticsServiceImpl statisticsService;
+
     @GetMapping
-    public String adminDashboard() {
+    public String adminDashboard(Model model) {
+        model.addAttribute("stats", statisticsService.getAdminDashboardStats());
         return "admin/dashboard";
     }
 }
