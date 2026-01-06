7244a7201037afd12c8b66d418ee52788f0737b6
Ilyass bougati <i.bougati12@gmail.com>
2026-01-06 11:30:43 +0100

    Moving the interceptor to the filter module

--- server/DeepDame/src/main/java/com/deepdame/config/WebsocketSecurityConfig.java
+++ server/DeepDame/src/main/java/com/deepdame/config/WebsocketSecurityConfig.java
@@ -1,7 +1,7 @@
 package com.deepdame.config;
 
+import com.deepdame.filter.JwtWebSocketInterceptor;
 import lombok.RequiredArgsConstructor;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.messaging.Message;
 import org.springframework.messaging.handler.invocation.HandlerMethodArgumentResolver;
